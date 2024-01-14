eb71c8f8283d07356102b514b5b26842e2b1e8dd
ckorley4 <ckorley4uo@yahoo.com>
2024-01-14 00:44:20 -0500

    Running Test on React

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
 function Article() {
   return (
     <div>
       {/* please pass this test */}
+      <p>please pass this test </p>
     </div>
-  );
+  )
 }
 
-export default Article;
+export default Article
